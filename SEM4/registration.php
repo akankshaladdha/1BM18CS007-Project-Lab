e89401bd10c322d547d9295c580690b58c49d1c7
akankshaladdha <53806569+akankshaladdha@users.noreply.github.com>
2020-09-12 23:16:23 +0530

    Update registration.php

--- SEM4/registration.php
+++ SEM4/registration.php
@@ -1,4 +1,3 @@
-
 <?php
  
 session_start();
@@ -8,124 +7,143 @@ require_once("config.php");
 ?>
 
 <html>
+
     <head>
-		<title>Login Page</title>
+		<title>Registration</title>
+			
 		<script type="text/javascript">
-          function preventBack(){window.history.forward();}
+			function preventBack(){window.history.forward();}
             setTimeout("preventBack()",0);
             window.onunload=function(){null};
-		  </script>
-		  		  <link rel="stylesheet" type="text/css" href="registration.css">
+		</script>
 
-    </head>
+		<link rel="stylesheet" type="text/css" href="registration.css">
+
+	</head>
+	
 <body>
-	
-	<div>
-	<?php 
-
-	
-	if(isset($_POST['create']))
-    {
-
-        $db_host="localhost";
-        $db_user="root";
-        $db_pass="";
-        $db_name="self";
-
-        $con = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
- 
-	$firstname 		= $_POST['firstname'];
-	$lastname 		= $_POST['lastname'];
-	$emailid		= $_POST['email'];
-	$password 		= $_POST['password'];
-	$gender        =  $_POST['gender'];
-
-	$sql = " select * from registration where email='".$emailid."' limit 1";
-     $result= mysqli_query($con,$sql);
-     $row=mysqli_fetch_array($result);
-     $count=mysqli_num_rows($result);
-
-     if($row['email']==$emailid  && $count == 1)
-     {
-		 echo 'The entered email id already exists. Use other email id to register';
-     }
-     
-	   else{
-
-		$user_activation_code = rand(1000000,9999999);
-		$activation = $user_activation_code;
-		$sql = "INSERT INTO registration VALUES(?,?,?,?,?,?)";
-		$stmtinsert = $db->prepare($sql);
-		$result = $stmtinsert->execute([$firstname, $lastname, $emailid,  $password, $gender,$activation]);
-    if($result)
-		{
-			$subject="Email Verification link";
-			$to=$emailid;
-			$message =  " Welcome " . $firstname . " " .$lastname . " \n Please click on the following link to login.\n http://localhost/self/login1.php \n"  .$activation. "\n Enter this code to login.";
-			$from= 'nitesh.cs18@bmsce.ac.in';
-			mail($to, $subject, $message, 'From: nitesh.cs18@bmsce.ac.in');
-			echo  "<p style='
-			position: absolute;
-			top: 75%;z-index:1;
-			font-size: 15px;
-			margin-left: 510px;color:red;'>Verification code has been sent to ur registered email-id . Click on the link to log in to your account</p>";
-		}
-		else{
-			echo 'There were errors while saving the data in registration.';
-        }
-	 }
-}
-	?>
-  </div>
-  
  
 	<div id="id01" class="modal">
-  
-  <form class="modal-content animate" method="post">
-   <!--
- 	 <div class="imgcontainer">
-      
-      <img src="external.png"  class="avatar">
-    </div>
-  -->
-  <div class="head">
-    <h1>Registration</h1>
-  </div>
+		
+	<!--
+		<div class="imgcontainer">
+		<img src="external.png"  class="avatar">
+		</div>
+	-->
+	<div class="modal-content animate">
 
-    <div class="container">
-      <label for="firstname"><b>Firstname</b></label>
-	  <input class="select" type="text" placeholder="Enter Firstname" name="firstname" autocomplete=off required>
-	  
-	  <label for="lastname"><b>Lastname</b></label>
-      <input class="select" type="text" placeholder="Enter Lastname" name="lastname" autocomplete=off required>
-      
-      <label for="email"><b>Email Address</b></label>
-      <input class="select" type="email" placeholder="Enter Email" name="email" autocomplete=off required>  
+		<div class="heading">
+			<h1>Registration</h1>
+		</div>
 
-      <label for="password"><b>Password</b></label>
-	  <input class="select" type="password" placeholder="Enter Password" name="password" autocomplete=off required>
-	  
-	  <label><b>Gender :</b>
-			<input type="radio" name="gender" value="male">Male
-			<input type="radio" name="gender" value="female">Female
-			<input type="radio" name="gender" value="other">Other
-	  </label>
-	  
-        <div class="btn">
-		<button class="btn-success" type="submit" id="register" name="create">Sign Up</button>
-      </div>
-    </div>
+		<div class="err_msg">
+		
+		<?php 
+	
+			if(isset($_POST['create']))
+			{
+
+				$db_host="localhost";
+				$db_user="root";
+				$db_pass="";
+				$db_name="project_4";
+
+				$con = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+		
+				$firstname 		= $_POST['firstname'];
+				$lastname 		= $_POST['lastname'];
+				$emailid		= $_POST['email'];
+				$password 		= $_POST['password'];
+				$gender        =  $_POST['gender'];
+				$one = "akankshaladdha5@gmail.com";
+
+				$sql = " select * from registration where email='".$emailid."' limit 1";
+				$result= mysqli_query($con,$sql);
+				$row=mysqli_fetch_array($result);
+				$count=mysqli_num_rows($result);
+
+				if($row['email']==$emailid  && $count == 1)
+				{
+					echo 'The entered email-id is already registered with us. Please use a different email to create an account.';
+				}
+				
+				else{
+
+					$user_activation_code = rand(1000000,9999999);
+					$activation = $user_activation_code;
+					$sql = "INSERT INTO registration VALUES(?,?,?,?,?,?)";
+					$stmtinsert = $db->prepare($sql);
+					$result = $stmtinsert->execute([$firstname, $lastname, $emailid,  $password, $gender,$activation]);
+					
+					if($result)
+					{
+						if($one==$emailid)
+						{ 
+							$status="true";
+							$sql = "INSERT INTO login(fname,loginemail,password,status)values(?,?,?,?)";
+							$stmtinsert = $db->prepare($sql);
+							$result = $stmtinsert->execute([$firstname, $one,  $password, $status]);
+							if($result)
+							{
+								header('location:login.php');
+							}
+						}
+						else{
+						
+						$subject="Email Verification link";
+						$to=$emailid;
+						$message =  " Welcome " . $firstname . " " .$lastname . " \n Please click on the following link to login.\n http://localhost/self/login1.php \n"  .$activation. "\n Enter this code to login.";
+						$from= 'akankshaladdha0625@gmail.com';
+						mail($to, $subject, $message, 'From: akankshaladdha0625@gmail.com');
+						echo  "Verification code has been sent to ur registered email-id . Click on the link to log in to your account.";
+						}
+					}
+					else{
+						echo 'There were errors while saving the data in registration.';
+					}
+				}
+			}
+			?>
+
+		</div>
+
+		<form method="post" class="signup_btn">
+
+		<div class="container">
+			<div class="inputs">
+				<label for="firstname" class="labels"><b>Firstname</b></label><br>
+				<input class="select" type="text" placeholder="Enter Firstname" name="firstname" autocomplete="off" required>
+				<br>
+				<label for="lastname" class="labels"><b>Lastname</b></label><br>
+				<input class="select" type="text" placeholder="Enter Lastname" name="lastname" autocomplete="off" required>
+				<br>
+				<label for="email" class="labels"><b>Email Address</b></label><br>
+				<input class="select" type="email" placeholder="Enter Email" name="email" autocomplete="off" required>  
+				<br>
+				<label for="password" class="labels"><b>Password</b></label><br>
+				<input class="select" type="password" placeholder="Enter Password" name="password" autocomplete="off" required>
+				<br>
+				<label class="labels"><b>Gender :</b>
+					<input type="radio" name="gender" value="male">Male
+					<input type="radio" name="gender" value="female">Female
+					<input type="radio" name="gender" value="other">Other
+				</label>
+			</div>
+			<div class="btn">
+				<button class="btn-success" type="submit" id="register" name="create">Sign Up</button>
+			</div>
+		</div>
+		</form>
     
-    <div class="bottom" style="background-color:#D0D0D0">
-    <p class="line"><b>Already a member ?</b>
-      <button type="button" name="signup" class="cancelbtn" ><a href="login.php">Signin</a></button></p>
-    </div>
-  </form>
-</div>
+    	<div class="bottom">
+    		<p class="line"><b>Already a member ?</b>
+     		<button type="button" name="signup" class="cancelbtn" ><a href="login.php">Signin</a></button></p>
+    	</div>
+	</div>
+	</div>
 
-
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
 
 </body>
 </html>
